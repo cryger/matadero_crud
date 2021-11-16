87114bde1d5eaaa858fc0cf37d88e3230462b189
henry garcia <henry.garcia@convrsus.ai>
2021-11-16 00:05:45 -0500

    Agregado de nuevas funcionalidades y hojas de estilo

--- index.php
+++ index.php
@@ -1,45 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/bootstrap.css" />
     <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
     <script src="js/bootstrap.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <title>INDEX</title>
-  </head>
-  <body>
+</head>
+
+<body class="body">
     <nav class="navbar navbar-expand-lg navbar-warning bg-warning">
-      <a class="navbar-brand" href="#">MATADERO SALUIG</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="index.php"
-            >Home <span class="sr-only">(current)</span></a
-          >
-          <a class="nav-item nav-link" href="login.php">Login</a>
+        <a class="navbar-brand" href="#">MATADERO SALUIG</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="login.php">Login</a>
+            </div>
         </div>
-      </div>
     </nav>
     <!--Componentes de la pagina principal-->
-    <div class="container container-fluid">
-      <div class="row">
-        <div class="jumbotron mt-4">
-          <h2>Bienvenido al MATADERO SALUIG</h2>
+    <div class="card text-center">
+        <div class="card-header card-background">
+
+            <img src="img/54230948_101.jpg" alt="">
         </div>
-      </div>
+        <div class="card-body">
+            <h5 class="card-title">Matadero con la mejor carne y servicio al cliente</h5>
+            <p class="card-text">Ingresa a la plataforma presionando el boton a continuacion</p>
+            <a href="login.php" class="btn btn-primary">Ir al Dashboard</a>
+        </div>
+        <div class="card-footer text-muted">
+            Desarrollado por : Sara Restrepo - Luigi Pati;o
+        </div>
     </div>
-  </body>
+</body>
+
 </html>
--- login.php
+++ login.php
@@ -1,50 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/bootstrap.css" />
     <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
     <script src="js/bootstrap.js"></script>
     <script src="js/bootstrap.min.js"></script>
+
     <title>INDEX</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <nav class="navbar navbar-expand-lg navbar-warning bg-warning">
-      <a class="navbar-brand" href="#">MATADERO SALUIG</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="index.php"
-            >Home <span class="sr-only">(current)</span></a
-          >
-          <a class="nav-item nav-link" href="login.php">Login</a>
+        <a class="navbar-brand" href="#">MATADERO SALUIG</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="login.php">Login</a>
+            </div>
         </div>
-      </div>
     </nav>
     <!--Componentes de la pagina Login-->
-    <div class="container container-fluid container-responsive mt-4" >
-        <div class="row">
-            <form action="">
-                <div class="form-control">
-                    <h2>LOGIN</h2>
-                    <label for="">Usuario:</label>
-                    <input type="text">
+    <div class="container-login container m-0 row justify-content-center mt-4">
+        <div class="row col-auto ">
+            <form class="form-control">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
-    
-  </body>
+
+</body>
+
 </html>
